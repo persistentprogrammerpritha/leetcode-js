bca008a991a7d85cc0697ce8a49bd0a962bf362f
1persistentprogrammer <63676265+1persistentprogrammer@users.noreply.github.com>
2020-04-16 17:41:19 -0400

    Added Higher Space Solution
    /*Commented out higher space solution*/

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -18,3 +18,25 @@ var productExceptSelf = function(nums) {
     }
     return ans;
 };
+
+/* Below is the solution using 2 extra arrays
+var productExceptSelf = function(nums) {
+     //take the left product 
+    var leftArr = [];
+    var rightArr = [];
+    var ans = [];    
+    leftArr[0] = 1;
+    rightArr[nums.length - 1] = 1;
+    for(var i=1; i<nums.length; i++){
+        leftArr[i] = leftArr[i-1] * nums[i-1]; //1234 -> 1126
+    }
+    for(var i=nums.length-2; i>=0; i--){ //1234 -> 24,12,4,1
+        rightArr[i] = rightArr[i+1] * nums[i+1];
+    }
+    //console.log(rightArr)
+    for(var i=0; i<nums.length; i++){
+        ans[i] = leftArr[i] * rightArr[i];
+    }
+    return ans;
+};
+*/
