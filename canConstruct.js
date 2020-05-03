b3e460113ccceae970cb2ef2ef90b4308e6c7538
1persistentprogrammer <63676265+1persistentprogrammer@users.noreply.github.com>
2020-05-03 18:56:09 -0400

    Update canConstruct.js

--- canConstruct.js
+++ canConstruct.js
@@ -1,4 +1,25 @@
 var canConstruct = function(ransomNote, magazine) {
+    var magMap = {};
+     for(var i=0; i<magazine.length; i++){
+        var mchar = magazine[i]
+        if(magMap[mchar] == undefined){
+            magMap[mchar] = 1;
+        }
+        else{
+            magMap[mchar]++;
+        }
+    }
+    for(var i=0; i<ransomNote.length; i++){
+        var ch = ransomNote[i]
+        if(magMap[ch] == undefined || magMap[ch] == 0){
+            return false;
+        }
+        else{
+            magMap[ch]--;
+        }
+    }
+    return true;
+    /* Two Map Solution
     var ransomMap = {};
     var magMap = {};
     for(var i=0; i<ransomNote.length; i++){
@@ -19,6 +40,7 @@ var canConstruct = function(ransomNote, magazine) {
             magMap[mchar]++;
         }
     }
+    console.log(ransomMap)
     for(var i=0; i<ransomNote.length; i++){
         var ch = ransomNote[i];
         if(magMap[ch] == undefined || magMap[ch] < ransomMap[ch]){
@@ -26,4 +48,5 @@ var canConstruct = function(ransomNote, magazine) {
         }
     }
     return true;
+*/
 };
