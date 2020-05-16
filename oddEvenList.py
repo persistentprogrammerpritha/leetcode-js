d7f33eedeaeb78cc450c9d76b669a394f19f4dc5
1persistentprogrammer <63676265+1persistentprogrammer@users.noreply.github.com>
2020-05-16 10:43:15 -0400

    oddEvenList
    https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3331/

---
+++ oddEvenList.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if head == None: 
+            return None
+        odd = head
+        even = odd.next
+        even_head = even     
+        while even != None and even.next != None:
+            odd.next = even.next # connect 1 and 3 
+            odd = odd.next #move the odd pointer
+            even.next = odd.next #connect 2 to 4
+            even = even.next #move the even pointer
+        odd.next = even_head
+        return head
